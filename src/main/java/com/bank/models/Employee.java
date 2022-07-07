7cee9d22b80288716479bd264948ee573d517ab9
Erfan Saberi <erfansaberiow@gmail.com>
2022-07-07 18:43:52 +0430

    Added Employee Constructor and save method

--- src/main/java/com/bank/models/Employee.java
+++ src/main/java/com/bank/models/Employee.java
@@ -11,6 +11,21 @@ public class Employee extends Person {
     private String password; // Hashed
     private Date joinDate;
     private EmployeeStatus status;
+    
+    public Employee(String firstName, String lastName, String email, String rawPassword) {
+        super(firstName, lastName);
+        this.email = email;
+        this.password = hashPassword(rawPassword);
+        this.joinDate = new Date();
+        this.status = EmployeeStatus.ACTIVE;
+        this.id = allEmployees.size() + 1;
+    }
+
+    public void save() {
+        allEmployees.add(this);
+    }
+
+
 
 }
 
