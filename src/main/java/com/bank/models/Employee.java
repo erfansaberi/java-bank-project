8833fea7b4bee1d81eed3ee2250f6f167daf17d9
Erfan Saberi <erfansaberiow@gmail.com>
2022-07-07 16:47:22 +0430

    Added Employee fileds

--- src/main/java/com/bank/models/Employee.java
+++ src/main/java/com/bank/models/Employee.java
@@ -1,9 +1,26 @@
 package com.bank.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Employee {
     ArrayList<Employee> allEmployees = new ArrayList<>(); // All created accounts
 
     private long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Gender gender;
+    private Date joinDate;
+    private String nationalId;
+    private String password; // Hashed
+    private String email;
+    private Status status; // Active, Deactive, Deleted
+
+}
+
+enum Status {
+    Active,
+    Deactive,
+    Deleted
 }
---
+++ src/main/java/com/bank/models/Gender.java
@@ -0,0 +1,2 @@
+package com.bank.models;public enum Gender {
+}
