fd1f0b30b81f9e1d695cd212ead2eba663fc5122
Erfan Saberi <erfansmart12@gmail.com>
2022-07-09 10:47:19 +0430

    Customer authentication

--- src/main/java/com/bank/models/Customer.java
+++ src/main/java/com/bank/models/Customer.java
@@ -4,13 +4,175 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Customer extends Person {
-    ArrayList<Customer> allCustomers = new ArrayList<>(); // All created accounts
+    static ArrayList<Customer> allCustomers = new ArrayList<>(); // All created accounts
 
     private long id;
-    private String phoneNumber;
+    private String email;
+    private String phoneNumber; // Customer logs in with phone number
     private String password; // Hashed
     private Date joinDate;
     private CustomerStatus status;
+
+    /**
+     * Create new customer. Customer is not saved to arraylist.
+     * Save method must be called to save customer to arraylist.
+     * Id is generated automatically.
+     * Status is set to CustomerStatus.ACTIVE.
+     * Join date is set to current date.
+     */
+    public Customer() {
+        super();
+        this.joinDate = new Date();
+        this.status = CustomerStatus.ACTIVE;
+        this.id = allCustomers.size() + 1;
+    }
+
+    /**
+     * Save customer to arraylist.
+     */
+    public void save() {
+        allCustomers.add(this);
+    }
+
+    /**
+     * Check if phone number is used in arraylist.
+     * @param phoneNumber Phone number to check.
+     * @return True if phone number is used, false otherwise.
+     */
+    public static boolean phoneNumberExists(String phoneNumber) {
+        for (Customer customer : allCustomers) {
+            if (customer.getPhoneNumber().equals(phoneNumber)) {
+                if (customer.getStatus() != CustomerStatus.DELETED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if email is used in arraylist.
+     * @param email Email to check.
+     * @return True if email is used, false otherwise.
+     */
+    public static boolean emailExists(String email) {
+        for (Customer customer : allCustomers) {
+            if (customer.getEmail().equals(email)) {
+                if (customer.getStatus() != CustomerStatus.DELETED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get customer by phone number.
+     * @param phoneNumber Phone number to get customer by.
+     * @return Customer with given phone number, null if not found.
+     */
+    public static Customer getByPhoneNumber(String phoneNumber) {
+        for (Customer customer : allCustomers) {
+            if (customer.getPhoneNumber().equals(phoneNumber)) {
+                if (customer.getStatus() != CustomerStatus.DELETED) {
+                    return customer;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get customer by email.
+     * @param email Email to get customer by.
+     * @return Customer with given email, null if not found.
+     */
+    public static Customer getByEmail(String email) {
+        for (Customer customer : allCustomers) {
+            if (customer.getEmail().equals(email)) {
+                if (customer.getStatus() != CustomerStatus.DELETED) {
+                    return customer;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Authenticate customer.
+     * @param phoneNumber Phone number entered by user.
+     * @param rawPassword Password entered by user.
+     * @return true if phone number and password are correct, false otherwise
+     */
+    public static boolean authenticate(String phoneNumber, String rawPassword) {
+        if (phoneNumberExists(phoneNumber)) {
+            Customer customer = Customer.getByPhoneNumber(phoneNumber);
+            if (customer.getPassword().equals(Core.hashPassword(rawPassword))) {
+                if (customer.getStatus() == CustomerStatus.ACTIVE) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.password = passwordHash;
+    }
+
+    public void setPassword(String rawPassword) {
+        this.password = Core.hashPassword(rawPassword);
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public CustomerStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CustomerStatus status) {
+        this.status = status;
+    }
+
+    public static ArrayList<Customer> getAllCustomers() {
+        return allCustomers;
+    }
+
 }
 
 enum CustomerStatus {
--- src/test/java/com/bank/models/CustomerTest.java
+++ src/test/java/com/bank/models/CustomerTest.java
@@ -16,12 +16,19 @@ class CustomerTest {
     static String testEmailInactive = "inactive@ut.ac.ir";
     static String testEmailBanned = "banned@ut.ac.ir";
     static String testEmailDeleted = "deleted@ut.ac.ir";
+    static String testPhoneNumberActive = "09121234567";
+    static String testPhoneNumberPending = "09121234568";
+    static String testPhoneNumberInactive = "09121234569";
+    static String testPhoneNumberBanned = "09121234570";
+    static String testPhoneNumberDeleted = "09121234571";
     static String testPassword = "HESOYAM";
 
     @BeforeAll
     static void setUp() {
+        Core.loadConfig();
         // Create a pending Customer
         pendingCustomer = new Customer();
+        pendingCustomer.setPhoneNumber(testPhoneNumberPending);
         pendingCustomer.setEmail(testEmailPending);
         pendingCustomer.setPassword(testPassword);
         pendingCustomer.setStatus(CustomerStatus.PENDING);
@@ -29,26 +36,30 @@ class CustomerTest {
 
         // Create an active Customer
         activeCustomer = new Customer();
+        activeCustomer.setPhoneNumber(testPhoneNumberActive);
         activeCustomer.setEmail(testEmailActive);
         activeCustomer.setPassword(testPassword);
         activeCustomer.save();
 
         // Create an inactive Customer
         inactiveCustomer = new Customer();
+        inactiveCustomer.setPhoneNumber(testPhoneNumberInactive);
         inactiveCustomer.setEmail(testEmailInactive);
         inactiveCustomer.setPassword(testPassword);
         inactiveCustomer.setStatus(CustomerStatus.INACTIVE);
         inactiveCustomer.save();
 
         // Create a banned Customer
-        inactiveCustomer = new Customer();
-        inactiveCustomer.setEmail(testEmailInactive);
-        inactiveCustomer.setPassword(testPassword);
-        inactiveCustomer.setStatus(CustomerStatus.BANNED);
-        inactiveCustomer.save();
+        bannedCustomer = new Customer();
+        bannedCustomer.setPhoneNumber(testPhoneNumberBanned);
+        bannedCustomer.setEmail(testEmailBanned);
+        bannedCustomer.setPassword(testPassword);
+        bannedCustomer.setStatus(CustomerStatus.BANNED);
+        bannedCustomer.save();
 
         // Create a deleted Customer
         deletedCustomer = new Customer();
+        deletedCustomer.setPhoneNumber(testPhoneNumberDeleted);
         deletedCustomer.setEmail(testEmailDeleted);
         deletedCustomer.setPassword(testPassword);
         deletedCustomer.setStatus(CustomerStatus.DELETED);
@@ -72,12 +83,12 @@ class CustomerTest {
 
     @Test
     void testCustomerAuthenticate() {
-        assertTrue(Customer.authenticate(testEmailActive, testPassword));
+        assertTrue(Customer.authenticate(testPhoneNumberActive, testPassword));
         assertFalse(Customer.authenticate("anotherEmail@ut.ac.ur", testPassword));
         assertFalse(Customer.authenticate(testEmailActive, "anotherPassword"));
-        assertFalse(Customer.authenticate(testEmailPending, testPassword));
-        assertFalse(Customer.authenticate(testEmailInactive, testPassword));
-        assertFalse(Customer.authenticate(testEmailBanned, testPassword));
-        assertFalse(Customer.authenticate(testEmailDeleted, testPassword));
+        assertFalse(Customer.authenticate(testPhoneNumberPending, testPassword));
+        assertFalse(Customer.authenticate(testPhoneNumberInactive, testPassword));
+        assertFalse(Customer.authenticate(testPhoneNumberBanned, testPassword));
+        assertFalse(Customer.authenticate(testPhoneNumberDeleted, testPassword));
     }
 }
