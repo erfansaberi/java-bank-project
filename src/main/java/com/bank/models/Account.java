3a02980a06714d07a8c4462dd4fe68ad842eb74e
Erfan Saberi <erfansmart12@gmail.com>
2022-07-10 15:26:11 +0430

    Account class, getter and setters and methods

--- src/main/java/com/bank/models/Account.java
+++ src/main/java/com/bank/models/Account.java
@@ -1,22 +1,121 @@
 package com.bank.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Account {
-    ArrayList<Account> allAccounts = new ArrayList<>(); // All created accounts
+    static ArrayList<Account> allAccounts = new ArrayList<>(); // All created accounts
 
     private long id;
+    private Customer owner;
+    private double balance;
+    private Date creationDate;
+    private AccountStatus status;
+
+    public Account() {
+        this.id = allAccounts.size() + 1;
+        this.balance = 0;
+        this.creationDate = new Date();
+        this.status = AccountStatus.ACTIVE;
+    }
+
+    public Account(Customer owner, double balance) {
+        this.id = allAccounts.size() + 1;
+        this.owner = owner;
+        this.balance = balance;
+        this.creationDate = new Date();
+        this.status = AccountStatus.ACTIVE;
+    }
+
+    public void save() {
+        allAccounts.add(this);
+    }
+
+    public static boolean idExists(long id) {
+        for (Account account : allAccounts) {
+            if (account.getId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Account getAccountById(long id) {
+        for (Account account : allAccounts) {
+            if (account.getId() == id) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+
+    public static ArrayList<Account> getAllAccounts() {
+        return allAccounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Account {" +
+                "id=" + id +
+                ", owner=" + owner.getFullNameWithId() +
+                ", balance=" + balance +
+                ", creationDate=" + creationDate +
+                ", status=" + status +
+                '}';
+    }
+
 }
 
 enum AccountStatus {
-    Pending,
-    Active,
-    Deactivated,
-    Deleted
+    PENDING,
+    ACTIVE,
+    DEACTIVATED,
+    DELETED
 }
 
 enum AccountType {
-    Transaction,
-    LongTerm,
-    ShortTerm
+    TRANSACTION,
+    LONGTERM,
+    SHORTTERM
 }
