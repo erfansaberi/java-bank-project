992b7cb4f87938771e0c0cfcad04d4e59c60c91c
Erfan Saberi <erfansmart12@gmail.com>
2022-07-10 15:09:27 +0430

    Customer Register

--- src/main/java/com/bank/validator/Validator.java
+++ src/main/java/com/bank/validator/Validator.java
@@ -1,5 +1,9 @@
 package com.bank.validator;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Validator {
     /**
      * Validates the given email address by regular expression.
@@ -31,5 +35,21 @@ public class Validator {
      */
     public static boolean isValidPhoneNumber(String phoneNumber) {
         return phoneNumber.matches("^[0-9]{11}$");
+    }
+
+    /**
+     * Validates the given date to be in the format YYYY-MM-DD
+     * by parsing the given date string with the given format
+     * in a try-catch block.
+     * @param birthDateStr
+     * @return true if the date is valid, false otherwise.
+     */
+    public static boolean isValidBirthDate(String birthDateStr) {
+        try {
+            Date birthDate = new SimpleDateFormat("yyyy-MM-dd").parse(birthDateStr);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
