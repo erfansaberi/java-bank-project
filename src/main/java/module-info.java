44dfab4b19e4eb10e3bd41de3bca4841b77d4319
Erfan Saberi <erfansmart12@gmail.com>
2022-07-12 11:00:14 +0430

    Minor changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.bank.gui {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
@@ -14,7 +13,6 @@ module com.bank.gui {
     
     
     opens com.bank.gui to javafx.fxml;
-//    opens com.bank.gui to javafx.base;
     opens com.bank.models to javafx.base;
     exports com.bank.gui;
 }
